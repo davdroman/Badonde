122c438630ebaef68c5ad4340b097dc5053df413
DavdRoman <d@vidroman.me>
2018-09-28 11:30:51 +0100

    Add CLI entry point

--- Package.swift
+++ Package.swift
@@ -5,18 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "Badonde",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Badonde",
-            dependencies: []),
-        .testTarget(
-            name: "BadondeTests",
-            dependencies: ["Badonde"]),
+            dependencies: ["BadondeCore"]
+        ),
+        .target(name: "BadondeCore")
     ]
 )
--- Sources/Badonde/main.swift
+++ Sources/Badonde/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import BadondeCore
+
+let tool = CommandLineTool()
+
+do {
+	try tool.run()
+} catch {
+	print("Whoops! An error occurred: \(error)")
+}
