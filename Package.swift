2177d91bdfe8ae27d104001d32eff66102826dbf
DavdRoman <d@vidroman.me>
2018-09-28 11:17:57 +0100

    Reinit package

--- Package.swift
+++ Package.swift
@@ -1,37 +1,22 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-	name: "Badonde",
-	dependencies: [
-		.package(
-			url: "https://github.com/nvzqz/FileKit",
-			from: "5.0.0"
-		),
-		.package(
-			url: "https://github.com/JohnSundell/ShellOut",
-			from: "2.1.0"
-		),
-		.package(
-			url: "https://github.com/Moya/Moya",
-			from: "10.0.1"
-		)
-	],
-	targets: [
-		.target(
-			name: "Badonde",
-			dependencies: ["BadondeCore"]
-		),
-		.target(
-			name: "BadondeCore",
-			dependencies: ["FileKit", "ShellOut", "Moya"]
-		),
-		.testTarget(
-			name: "BadondeTests",
-			dependencies: ["BadondeCore", "FileKit"]
-		)
-	]
+    name: "Badonde",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Badonde",
+            dependencies: []),
+        .testTarget(
+            name: "BadondeTests",
+            dependencies: ["Badonde"]),
+    ]
 )
-
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import BadondeTests
+
+var tests = [XCTestCaseEntry]()
+tests += BadondeTests.allTests()
+XCTMain(tests)
