b8d3073133f8df6908a8fa8f37554dbb9a3544cf
DavdRoman <d@vidroman.me>
2019-02-04 18:44:21 +0000

    Add `BadondeCommand`

--- Sources/BadondeCore/CommandLineTool.swift
+++ Sources/BadondeCore/CommandLineTool.swift
@@ -1,34 +1,6 @@
 import Foundation
 import SwiftCLI
 
-class BadondeCommand: Command {
-	let name = ""
-
-	enum Error: Swift.Error {
-		case urlInvalid
-	}
-
-	func execute() throws {
-		guard let currentBranch = try? capture(bash: "git rev-parse --abbrev-ref HEAD").stdout else {
-			return
-		}
-
-		let pullRequestURLFactory = PullRequestURLFactory(repositoryShorthand: "asosteam/asos-native-ios")
-		pullRequestURLFactory.baseBranch = "develop"
-		pullRequestURLFactory.targetBranch = currentBranch
-		pullRequestURLFactory.title = "Something in the air"
-		pullRequestURLFactory.labels = ["Bug", "Back in Stock"]
-
-		guard let pullRequestURL = pullRequestURLFactory.url else {
-			throw Error.urlInvalid
-		}
-
-		try run(bash: "open \"\(pullRequestURL)\"")
-
-		stdout <<< currentBranch
-	}
-}
-
 public final class CommandLineTool {
     private let arguments: [String]
 
