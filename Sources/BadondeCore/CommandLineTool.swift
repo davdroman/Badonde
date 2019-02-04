249f94605d6a4fa3723ec698657ca8ec1719f2a8
davdroman <d@vidroman.dev>
2019-02-04 18:39:21 +0000

    Add `PullRequestURLFactory.swift`

--- Sources/BadondeCore/CommandLineTool.swift
+++ Sources/BadondeCore/CommandLineTool.swift
@@ -19,50 +19,6 @@ extension Array {
 	}
 }
 
-class PullRequestURLFactory: Codable {
-
-	var repositoryShorthand: String
-	var baseBranch: String?
-	var targetBranch: String?
-	var title: String?
-	var labels: [String]?
-
-	init(repositoryShorthand: String) {
-		self.repositoryShorthand = repositoryShorthand
-	}
-
-	var url: URL? {
-		var urlComponents = URLComponents()
-		urlComponents.scheme = "https"
-		urlComponents.host = "github.com"
-		urlComponents.path = urlPath
-
-		urlComponents.queryItems = [
-			URLQueryItem(name: CodingKeys.title.stringValue, mandatoryValue: title),
-			URLQueryItem(name: CodingKeys.labels.stringValue, mandatoryValue: labels?.joined(separator: ","))
-		].compactMap({ $0 }).nilIfEmpty
-
-		return urlComponents.url
-	}
-
-	private var urlPath: String {
-		var path = "/\(repositoryShorthand)/compare"
-
-		switch (baseBranch, targetBranch) {
-		case let (base?, target?):
-			path.append(contentsOf: "/\(base)...\(target)")
-		case let (nil, target?):
-			path.append(contentsOf: "/\(target)")
-		case let (base?, nil):
-			path.append(contentsOf: "/\(base)...")
-		case (nil, nil):
-			path.append(contentsOf: "/")
-		}
-
-		return path
-	}
-}
-
 class BadondeCommand: Command {
 	let name = ""
 
