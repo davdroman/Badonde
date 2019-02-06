c45eec59d5c626317576f6fa0dd42f5252404344
davdroman <d@vidroman.dev>
2019-02-06 20:20:24 +0000

    Add `SwiftyStringScore+Sugar`

---
+++ Sources/BadondeCore/SwiftyStringScore+Sugar.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftyStringScore+Sugar.swift
+//  BadondeCore
+//
+//  Created by David Roman on 06/02/2019.
+//
+
+import SwiftyStringScore
+
+extension Array where Element == String {
+	func fuzzyMatch(word: String) -> String? {
+		return map { (score: $0.score(word: word, fuzziness: 1), string: $0) }
+			.filter { $0.score > 0.5 }
+			.sorted { $0.score > $1.score }
+			.first?
+			.string
+	}
+}
