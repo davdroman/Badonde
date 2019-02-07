07300224f3113beab9992144a5306e5c08c38f79
DavdRoman <d@vidroman.me>
2019-02-07 12:22:16 +0000

    Add CLI details and commands

--- Sources/BadondeCore/CommandLineTool.swift
+++ Sources/BadondeCore/CommandLineTool.swift
@@ -2,14 +2,19 @@ import Foundation
 import SwiftCLI
 
 public final class CommandLineTool {
-    private let arguments: [String]
+	private let arguments: [String]
 
-    public init(arguments: [String] = CommandLine.arguments) {
-        self.arguments = arguments
-    }
+	public init(arguments: [String] = CommandLine.arguments) {
+		self.arguments = arguments
+	}
 
-    public func run() throws {
-        let cli = CLI(singleCommand: BadondeCommand())
+	public func run() throws {
+		let cli = CLI(
+			name: "badonde",
+			version: "1.0.0",
+			description: "Effortless PR creation too",
+			commands: [BurghCommand(), ClearCommand()]
+		)
 		_ = cli.go()
-    }
+	}
 }
