be950cc12c70a1e2ec7b9f1d594e78ee042f6271
DavdRoman <d@vidroman.me>
2019-02-07 12:20:25 +0000

    Add `ClearCommand`

---
+++ Sources/BadondeCore/ClearCommand.swift
@@ -0,0 +1,26 @@
+//
+//  ClearCommand.swift
+//  BadondeCore
+//
+//  Created by David Roman on 07/02/2019.
+//
+
+import Foundation
+import SwiftCLI
+
+class ClearCommand: Command {
+	let name = "clear"
+	let shortDescription = "Clears credentials"
+
+	func execute() throws {
+		defer {
+			exit(EXIT_SUCCESS)
+		}
+
+		let accessTokenStore = AccessTokenStore()
+		guard accessTokenStore.config != nil else {
+			return
+		}
+		accessTokenStore.config = nil
+	}
+}
