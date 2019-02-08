5df1cd1e7d8e875a45ecbea6cddf61d2b363e12b
davdroman <d@vidroman.dev>
2019-02-08 10:05:54 +0000

    Remove `Result` dependency

--- Package.swift
+++ Package.swift
@@ -14,10 +14,6 @@ let package = Package(
 			from: "5.0.0"
 		),
 		.package(
-			url: "https://github.com/antitypical/Result",
-			from: "4.0.0"
-		),
-		.package(
 			url: "https://github.com/DavdRoman/SwiftyStringScore",
 			.branch("master")
 		)
@@ -29,7 +25,7 @@ let package = Package(
 		),
 		.target(
 			name: "BadondeCore",
-			dependencies: ["SwiftCLI", "Result", "SwiftyStringScore"]
+			dependencies: ["SwiftCLI", "SwiftyStringScore"]
 		)
 	]
 )
