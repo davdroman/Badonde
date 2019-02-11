77db5b8f9dbfbc9607608ae6c7e93e8cb25d0101
davdroman <d@vidroman.dev>
2019-02-11 18:31:38 +0000

    Implement analytics reporting

--- Sources/BadondeCore/PullRequestAnalyticsReporter.swift
+++ Sources/BadondeCore/PullRequestAnalyticsReporter.swift
@@ -1,18 +1,48 @@
 import Foundation
 
+struct PullRequestAnalyticsData: Codable {
+	var isDependent: Bool
+	var labelCount: Int
+	var hasMilestone: Bool
+}
+
 final class PullRequestAnalyticsReporter {
 
-	private let firebaseApiToken: String
-
-	var isDependent: Bool = false
-	var labelCount: Int = 0
-	var hasMilestone: Bool = false
-
-	init(firebaseApiToken: String) {
-		self.firebaseApiToken = firebaseApiToken
+	enum Error: Swift.Error {
+		case urlFormattingError
+		case firebaseConnection
+		case noDataReceived
 	}
 
-	func report() {
-		// TODO: implement
+	private let firebaseProjectId: String
+	private let firebaseSecretToken: String
+
+	init(firebaseProjectId: String, firebaseSecretToken: String) {
+		self.firebaseProjectId = firebaseProjectId
+		self.firebaseSecretToken = firebaseSecretToken
+	}
+
+	func report(_ pullRequestData: PullRequestAnalyticsData) throws {
+		let reportUrl = URL(
+			scheme: "https",
+			host: "\(firebaseProjectId).firebaseio.com",
+			path: "/pull-requests.json",
+			queryItems: [URLQueryItem(name: "auth", value: firebaseSecretToken)]
+		)
+
+		guard let url = reportUrl else {
+			throw Error.urlFormattingError
+		}
+
+		let session = URLSession(configuration: .default)
+		var request = URLRequest(url: url)
+		request.httpMethod = "POST"
+		request.httpBody = try JSONEncoder().encode(pullRequestData)
+
+		let response = session.synchronousDataTask(with: request)
+
+		guard response.error == nil else {
+			throw Error.firebaseConnection
+		}
 	}
 }
