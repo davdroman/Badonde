0a6020be11e3131a244b8aa59dd9c6dbcb7f96b6
DavdRoman <d@vidroman.me>
2019-02-11 18:38:00 +0000

    Pack `burgh` alias on installation step

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 
 swift build -c release -Xswiftc -static-stdlib
 cp -f .build/release/badonde /usr/local/bin/badonde
+echo "#!/bin/bash" > /usr/local/bin/burgh
+echo "badonde burgh" >> /usr/local/bin/burgh
+chmod +x /usr/local/bin/burgh
