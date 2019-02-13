10a02d6136351e282fed582b9004027086bb8857
DavdRoman <d@vidroman.me>
2019-02-13 11:32:43 +0000

    Read passed arguments for `burgh` alias

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 swift build -c release -Xswiftc -static-stdlib
 cp -f .build/release/badonde /usr/local/bin/badonde
 echo "#!/bin/bash" > /usr/local/bin/burgh
-echo "badonde burgh" >> /usr/local/bin/burgh
+echo 'badonde burgh "$@"' >> /usr/local/bin/burgh
 chmod +x /usr/local/bin/burgh
