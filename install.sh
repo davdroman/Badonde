c4f4481abc0e44d4686e24c6a1684d554f517aea
davdroman <d@vidroman.dev>
2019-02-15 16:29:26 +0000

    Add Mint and Make installation methods

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-swift build -c release -Xswiftc -static-stdlib
-cp -f .build/release/badonde /usr/local/bin/badonde
-echo "#!/bin/bash" > /usr/local/bin/burgh
-echo 'badonde burgh "$@"' >> /usr/local/bin/burgh
-chmod +x /usr/local/bin/burgh
