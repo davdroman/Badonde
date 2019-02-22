076196a5be269703d9b8f11874079fc894add8ec
David Roman <d@vidroman.me>
2019-02-22 16:08:41 +0000

    Revert "Add step logging to `clear` and `set-firebase-auth` commands"
    This reverts commit afb0342fa5717a011d2843902dd1c9dc3bda806a.

--- Sources/BadondeCore/ClearCommand.swift
+++ Sources/BadondeCore/ClearCommand.swift
@@ -6,16 +6,10 @@ class ClearCommand: Command {
 	let shortDescription = "Clears credentials"
 
 	func execute() throws {
-		defer { Logger.fail() } // defers failure call if `Logger.finish()` isn't called at the end, which means an error was thrown throughout the codepath
-
-		Logger.step("Removing existing configuration")
 		let store = ConfigurationStore()
 		guard store.configuration != nil else {
-			Logger.info("No existing configuration found")
 			return
 		}
 		try store.clearConfiguration()
-
-		Logger.finish()
 	}
 }
