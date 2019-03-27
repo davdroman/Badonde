0f77b75ea2fe0fd442c3f8eb9b3bcc008acc8250
davdroman <d@vidroman.dev>
2019-03-27 11:13:05 +0000

    Re-link `ProcessError` descriptions

--- Sources/BadondeCore/Sugar/ProcessError+Sugar.swift
+++ Sources/BadondeCore/Sugar/ProcessError+Sugar.swift
@@ -1,5 +1,11 @@
 import SwiftCLI
 
+extension ProcessError where Self: Swift.Error {
+	var message: String? {
+		return localizedDescription
+	}
+}
+
 extension ProcessError {
 	var exitStatus: Int32 {
 		return 1
