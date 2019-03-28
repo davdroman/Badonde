f1e686c6f48d2a71c97c60e97b568de657fa8fa5
davdroman <d@vidroman.dev>
2019-03-28 17:01:55 +0000

    Bump version number (1.5.0)

--- Sources/BadondeCore/CommandLineTool.swift
+++ Sources/BadondeCore/CommandLineTool.swift
@@ -8,7 +8,7 @@ public final class CommandLineTool {
 	public func run(with arguments: [String]? = nil) {
 		let cli = CLI(
 			name: "badonde",
-			version: "1.4.2",
+			version: "1.5.0",
 			description: "Painless PR-ing",
 			commands: [
 				AppifyCommand(),
