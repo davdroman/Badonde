f9b2e2b1944116a08d460f89079e87a438b5e251
davdroman <d@vidroman.dev>
2019-03-29 13:44:24 +0000

    Handle GitHub API error responses

--- Sources/BadondeCore/BurghCommandError.swift
+++ Sources/BadondeCore/BurghCommandError.swift
@@ -15,17 +15,17 @@ extension BurghCommand.Error: Swift.Error {
 	var localizedDescription: String {
 		switch self {
 		case .noGitRepositoryFound:
-			return "☛ No Git repository found in current directory"
+			return "No Git repository found in current directory"
 		case .invalidBaseBranch(let branch):
-			return "☛ No remote branch found matching specified term '\(branch)'"
+			return "No remote branch found matching specified term '\(branch)'"
 		case .invalidBranchFormat(let branch):
-			return "☛ Invalid ticket format for current branch '\(branch)'"
+			return "Invalid ticket format for current branch '\(branch)'"
 		case .noTicketKey:
-			return "☛ Current branch is a 'NO-TICKET', please use a ticket prefixed branch"
+			return "Current branch is a 'NO-TICKET', please use a ticket prefixed branch"
 		case .missingGitRemote:
-			return "☛ Git remote named 'origin' is missing, please add it with `git remote add origin {git_url}`"
+			return "Git remote named 'origin' is missing, please add it with `git remote add origin {git_url}`"
 		case .invalidPullRequestURL:
-			return "☛ Could not form a valid pull request URL"
+			return "Could not form a valid pull request URL"
 		}
 	}
 }
--- Sources/GitHub/APIError.swift
+++ Sources/GitHub/APIError.swift
@@ -1,19 +1,22 @@
 import Foundation
 
 extension API {
-	public enum Error {
-		case githubConnectionFailed(Swift.Error)
-		case noDataReceived(Any.Type)
+	public struct Error {
+		var message: String
+		var documentationUrl: String?
+	}
+}
+
+extension API.Error: Codable {
+	enum CodingKeys: String, CodingKey {
+		case message
+		case documentationUrl = "documentation_url"
 	}
 }
 
 extension API.Error: Swift.Error {
 	public var localizedDescription: String {
-		switch self {
-		case .githubConnectionFailed(let error):
-			return "☛ GitHub API call failed with error: \(error)"
-		case .noDataReceived(let modelType):
-			return "☛ No data received for GitHub API call for type '\(modelType)'"
-		}
+		let reference = documentationUrl.map { " - refer to \($0)" } ?? ""
+		return "GitHub API call failed with error: \(message)" + reference
 	}
 }
