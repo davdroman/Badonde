3f577511de72d103cb1ea1602fd11662303c32b0
davdroman <d@vidroman.dev>
2019-03-29 16:48:50 +0000

    Limit `appify` template download to `<= current version`

--- Sources/BadondeCore/CommandLineTool.swift
+++ Sources/BadondeCore/CommandLineTool.swift
@@ -3,13 +3,19 @@ import SwiftCLI
 
 public final class CommandLineTool {
 
+	enum Constant {
+		static let name = "badonde"
+		static let version = "1.5.1"
+		static let description = "Painless PR-ing"
+	}
+
 	public init() {}
 
 	public func run(with arguments: [String]? = nil) {
 		let cli = CLI(
-			name: "badonde",
-			version: "1.5.1",
-			description: "Painless PR-ing",
+			name: Constant.name,
+			version: Constant.version,
+			description: Constant.description,
 			commands: [
 				AppifyCommand(),
 				BurghCommand(),
