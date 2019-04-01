e304481cdc2379c5eea071865fe02bea53905bb3
davdroman <d@vidroman.dev>
2019-04-01 18:55:22 +0100

    Refactor `PullRequestAnalyticsReporter`

--- Sources/BadondeCore/PullRequestAnalyticsReporterError.swift
+++ Sources/BadondeCore/PullRequestAnalyticsReporterError.swift
@@ -1,12 +1,13 @@
 import Foundation
+import GitHub
 
-extension PullRequestAnalyticsReporter {
+extension PullRequest.AnalyticsReporter {
 	enum Error {
 		case http(Int)
 	}
 }
 
-extension PullRequestAnalyticsReporter.Error: Swift.Error {
+extension PullRequest.AnalyticsReporter.Error: Swift.Error {
 	var localizedDescription: String {
 		switch self {
 		case .http(let statusCode):
