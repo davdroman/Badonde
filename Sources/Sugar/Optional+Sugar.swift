c591d57598a79503f17b7f89bb023c844a3bb38c
davdroman <d@vidroman.dev>
2019-04-09 10:41:11 +0100

    Clean up unneeded files

--- Sources/Sugar/Optional+Sugar.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Optional where Wrapped: RangeReplaceableCollection {
-	public mutating func append(_ newElement: Wrapped.Element) {
-		var collection = self ?? Wrapped()
-		collection.append(newElement)
-		self = collection
-	}
-}
