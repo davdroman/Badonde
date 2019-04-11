fa4ee77e3a65020d963fe3b908448b27cf9279e4
davdroman <d@vidroman.dev>
2019-04-11 12:37:25 +0100

    Implement type-safe `Repository.Shorthand`

--- Sources/GitHub/ReleaseAPI.swift
+++ Sources/GitHub/ReleaseAPI.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension Release {
 	public final class API: GitHub.API {
-		public func getReleases(for shorthand: String) throws -> [Release] {
+		public func getReleases(for shorthand: Repository.Shorthand) throws -> [Release] {
 			return try get(
 				[Release].self,
 				for: shorthand,
