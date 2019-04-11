04bd59424666f0619b69d78d91d9841c01e1f4c9
davdroman <d@vidroman.dev>
2019-04-11 21:13:40 +0100

    Implement `URL.homeDirectory()` func

--- Sources/Sugar/URL+Sugar.swift
+++ Sources/Sugar/URL+Sugar.swift
@@ -1,6 +1,14 @@
 import Foundation
 
 extension URL {
+	public static func homeDirectory() throws -> URL {
+		guard #available(OSX 10.12, *) else {
+			throw Error.minimumVersionOfMacOSRequired10_2
+		}
+
+		return FileManager().homeDirectoryForCurrentUser
+	}
+
 	public init(scheme: String, host: String, path: String, queryItems: [URLQueryItem]? = nil) throws {
 		var urlComponents = URLComponents()
 		urlComponents.scheme = scheme
@@ -20,6 +28,7 @@ extension URL {
 
 extension URL {
 	public enum Error {
+		case minimumVersionOfMacOSRequired10_2
 		case invalidURL(URLComponents)
 	}
 }
@@ -27,6 +36,8 @@ extension URL {
 extension URL.Error: Swift.Error {
 	public var localizedDescription: String {
 		switch self {
+		case .minimumVersionOfMacOSRequired10_2:
+			return "This command is not supported on versions of macOS lower than 10.12"
 		case .invalidURL(let urlComponents):
 			return ["URL formatting failed. Info:", "\(urlComponents)"].joined(separator: "\n")
 		}
