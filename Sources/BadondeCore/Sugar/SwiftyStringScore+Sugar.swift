e7c8922251c090e12bd8422330fa73dc4bfc8055
davdroman <d@vidroman.me>
2019-04-16 16:47:14 +0100

    Fix term matching when word is longer than search term

--- Sources/BadondeCore/Sugar/SwiftyStringScore+Sugar.swift
+++ Sources/BadondeCore/Sugar/SwiftyStringScore+Sugar.swift
@@ -2,7 +2,7 @@ import SwiftyStringScore
 
 extension Array where Element == String {
 	func fuzzyMatch(word: String) -> String? {
-		return map { (score: $0.score(word: word, fuzziness: 1), string: $0) }
+		return map { (score: word.score(word: $0, fuzziness: 1), string: $0) }
 			.filter { $0.score > 0.5 }
 			.sorted { $0.score > $1.score }
 			.first?
