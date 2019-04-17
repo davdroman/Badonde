a6fdd7eff84531e2f497a49edd900c8ed4b527e6
davdroman <d@vidroman.dev>
2019-04-17 16:46:39 +0100

    Bump version number (1.8.4)

--- Sources/BadondeCore/CommandLineTool.swift
+++ Sources/BadondeCore/CommandLineTool.swift
@@ -5,7 +5,7 @@ public final class CommandLineTool {
 
 	enum Constant {
 		static let name = "badonde"
-		static let version = "1.8.3"
+		static let version = "1.8.4"
 		static let description = "Painless PR-ing"
 	}
 
