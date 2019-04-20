eaad9e8213dc49242ad91fb2fb568842e810cf4a
davdroman <d@vidroman.me>
2019-04-20 17:42:55 +0100

    Implement conformance of `Dictionary` and `Array` to `URLContentInitializable`

--- Sources/TestSugar/FixtureLoadable.swift
+++ Sources/TestSugar/FixtureLoadable.swift
@@ -1,10 +1,50 @@
 import Foundation
 
 public protocol URLContentInitializable {
-	init(contentsOf: URL) throws
+	init(contentsOf url: URL) throws
 }
 
 extension String: URLContentInitializable {}
+
+extension Data: URLContentInitializable {
+	public init(contentsOf url: URL) throws {
+		self = try Data(contentsOf: url, options: [])
+	}
+}
+
+extension Dictionary {
+	init(contentsOf url: URL) throws {
+		let data = try Data(contentsOf: url)
+		let object = try JSONSerialization.jsonObject(with: data, options: [])
+		guard let _self = object as? [Key: Value] else {
+			throw DecodingError.typeMismatch(
+				[Key: Value].self,
+				DecodingError.Context(
+					codingPath: [],
+					debugDescription: "Decoded fixture did not match expected type"
+				)
+			)
+		}
+		self = _self
+	}
+}
+
+extension Array {
+	init(contentsOf url: URL) throws {
+		let data = try Data(contentsOf: url)
+		let object = try JSONSerialization.jsonObject(with: data, options: [])
+		guard let _self = object as? [Element] else {
+			throw DecodingError.typeMismatch(
+				[Element].self,
+				DecodingError.Context(
+					codingPath: [],
+					debugDescription: "Decoded fixture did not match expected type"
+				)
+			)
+		}
+		self = _self
+	}
+}
 
 public protocol FixtureLoadable {
 	var sourceFilePath: String { get }
