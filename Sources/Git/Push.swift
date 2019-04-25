4fa06ec73612d78987be0fc61e06d7c733906c04
davdroman <d@vidroman.dev>
2019-04-25 12:12:44 +0100

    Implement and test `Push.perform(remote:branch:)`

---
+++ Sources/Git/Push.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public protocol PushInteractor {
+	func perform(remote: String, branch: String) throws
+}
+
+enum Push { }
+
+extension Push {
+	static func perform(remote: Remote, branch: Branch, interactor: PushInteractor? = nil) throws {
+		let interactor = interactor ?? SwiftCLI()
+		try interactor.perform(remote: remote.name, branch: branch.name)
+	}
+}
