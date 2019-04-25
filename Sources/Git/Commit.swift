4022d3e7c4e845678a814d2ff1440f83529a6995
davdroman <d@vidroman.dev>
2019-04-25 16:33:21 +0100

    Implement and test `Commit.count`

--- Sources/Git/Commit.swift
+++ Sources/Git/Commit.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public protocol CommitInteractor {
-	func count(baseBranch: String, targetBranch: String, after date: Date?) throws -> Int
+	func count(baseBranches: [String], targetBranch: String, after date: Date?) throws -> String
 }
 
 public struct Commit: Equatable {
@@ -31,5 +31,30 @@ public struct Commit: Equatable {
 		self.date = Date(timeIntervalSince1970: unixDate)
 		self.subject = subject
 		self.body = body
+	}
+}
+
+extension Commit {
+	public static func count(baseBranch: Branch, targetBranch: Branch, after date: Date? = nil, interactor: CommitInteractor? = nil) throws -> Int {
+		let interactor = interactor ?? SwiftCLI()
+		let commitCountRaw = try interactor.count(baseBranches: [baseBranch.fullName], targetBranch: targetBranch.name, after: date)
+		guard let commitCount = Int(commitCountRaw) else {
+			throw Error.numberNotFound
+		}
+		return commitCount
+	}
+
+	public static func count(baseBranches: [Branch], targetBranch: Branch, after date: Date? = nil, interactor: CommitInteractor? = nil) throws -> [(Branch, Int)] {
+		let interactor = interactor ?? SwiftCLI()
+		let commitCountRaw = try interactor.count(baseBranches: baseBranches.map { $0.fullName }, targetBranch: targetBranch.name, after: date)
+		return try commitCountRaw
+			.components(separatedBy: "\n")
+			.enumerated()
+			.map {
+				guard let count = Int($0.element) else {
+					throw Error.numberNotFound
+				}
+				return (baseBranches[$0.offset], count)
+			}
 	}
 }
