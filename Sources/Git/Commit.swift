0bb6611bb4748155db081d2c3807058b5dfe8b71
davdroman <d@vidroman.dev>
2019-04-26 14:30:49 +0100

    Remove `Commit` implementation and `Branch.latestCommit`

--- Sources/Git/Commit.swift
+++ Sources/Git/Commit.swift
@@ -4,35 +4,7 @@ public protocol CommitInteractor {
 	func count(baseBranches: [String], targetBranch: String, after date: Date?) throws -> String
 }
 
-public struct Commit: Equatable {
-	public var hash: String
-	public var author: User
-	public var date: Date
-	public var subject: String
-	public var body: String
-
-	public init(rawCommitContent: String) throws {
-		let boundary = "\n---BADONDE-BOUNDARY---\n"
-		let commitComponents = rawCommitContent.components(separatedBy: boundary)
-
-		guard
-			let hash = commitComponents.first,
-			let authorName = commitComponents.dropFirst().first,
-			let authorEmail = commitComponents.dropFirst(2).first,
-			let unixDate = commitComponents.dropFirst(3).first.flatMap(TimeInterval.init),
-			let subject = commitComponents.dropFirst(4).first,
-			let body = commitComponents.dropFirst(5).first
-		else {
-			throw Error.missingProperty
-		}
-
-		self.hash = hash
-		self.author = User(name: authorName, email: authorEmail)
-		self.date = Date(timeIntervalSince1970: unixDate)
-		self.subject = subject
-		self.body = body
-	}
-}
+public enum Commit { }
 
 extension Commit {
 	public static func count(baseBranch: Branch, targetBranch: Branch, after date: Date? = nil, interactor: CommitInteractor? = nil) throws -> Int {
