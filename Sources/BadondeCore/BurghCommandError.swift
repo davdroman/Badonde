84b13efa4e55ca4acb72d2cebe6e8c5834e9e1ee
davdroman <d@vidroman.me>
2019-04-26 13:47:11 +0100

    Power `BadondeCore` through `Git` module

--- Sources/BadondeCore/BurghCommandError.swift
+++ Sources/BadondeCore/BurghCommandError.swift
@@ -2,11 +2,10 @@ import Foundation
 
 extension BurghCommand {
 	enum Error {
-		case noGitRepositoryFound
+		case gitRemoteMissing
 		case invalidBaseBranch(String)
 		case invalidBranchFormat(String)
 		case noTicketKey
-		case missingGitRemote
 		case invalidPullRequestURL
 	}
 }
@@ -14,16 +13,14 @@ extension BurghCommand {
 extension BurghCommand.Error: Swift.Error {
 	var localizedDescription: String {
 		switch self {
-		case .noGitRepositoryFound:
-			return "No Git repository found in current directory"
+		case .gitRemoteMissing:
+			return "Git remote named 'origin' is missing, please add it with `git remote add origin {git_url}`"
 		case .invalidBaseBranch(let branch):
 			return "No remote branch found matching specified term '\(branch)'"
 		case .invalidBranchFormat(let branch):
 			return "Invalid ticket format for current branch '\(branch)'"
 		case .noTicketKey:
 			return "Current branch is a 'NO-TICKET', please use a ticket prefixed branch"
-		case .missingGitRemote:
-			return "Git remote named 'origin' is missing, please add it with `git remote add origin {git_url}`"
 		case .invalidPullRequestURL:
 			return "Could not form a valid pull request URL"
 		}
