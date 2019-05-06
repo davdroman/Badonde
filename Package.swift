0c61ac3f53449142354bc64fc8510373c1eb9794
davdroman <d@vidroman.me>
2019-05-06 16:58:41 +0100

    Bump minimum deployment target to macOS 10.13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "Badonde",
 	platforms: [
-		.macOS(.v10_12)
+		.macOS(.v10_13)
 	],
 	products: [
 		.executable(name: "badonde", targets: ["Badonde"]),
