3eb1739d9213bdef89484dd54b3e45f043601a5d
davdroman <d@vidroman.me>
2019-05-09 12:46:06 +0100

    Create config file if it does not exist when calling `badonde config`

--- Sources/Configuration/FileInteractor.swift
+++ Sources/Configuration/FileInteractor.swift
@@ -8,6 +8,9 @@ protocol JSONFileInteractor {
 extension Configuration {
 	final class FileInteractor: JSONFileInteractor {
 		func read(from url: URL) throws -> [String : Any] {
+			// TODO: remove when `badonde init` is implemented.
+			// https://github.com/davdroman/Badonde/issues/79
+			try createEmptyFileIfNeeded(for: url)
 			let data = try Data(contentsOf: url)
 			return (try? JSONSerialization.jsonObject(with: data, options: [])) as? [String: Any] ?? [:]
 		}
@@ -15,6 +18,18 @@ extension Configuration {
 		func write(_ rawObject: [String : Any], to url: URL) throws {
 			let data = try JSONSerialization.data(withJSONObject: rawObject, options: [.prettyPrinted, .sortedKeys])
 			try data.write(to: url)
+		}
+
+		private func createEmptyFileIfNeeded(for url: URL) throws {
+			if !FileManager.default.fileExists(atPath: url.path) {
+				let fileDirectoryURL = url.deletingLastPathComponent()
+				try FileManager.default.createDirectory(
+					at: fileDirectoryURL,
+					withIntermediateDirectories: true,
+					attributes: nil
+				)
+				try Data().write(to: url)
+			}
 		}
 	}
 }
