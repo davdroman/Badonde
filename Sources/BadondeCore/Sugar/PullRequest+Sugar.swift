304efb2fb96c27a4904255f29eb2e317fa1bb978
davdroman <d@vidroman.dev>
2019-05-17 12:43:14 +0100

    Gardening

--- Sources/BadondeCore/Sugar/PullRequest+Sugar.swift
+++ Sources/BadondeCore/Sugar/PullRequest+Sugar.swift
@@ -58,3 +58,18 @@ extension PullRequest {
 		}
 	}
 }
+
+extension PullRequest.AnalyticsReporter {
+	enum Error {
+		case http(Int)
+	}
+}
+
+extension PullRequest.AnalyticsReporter.Error: Swift.Error {
+	var localizedDescription: String {
+		switch self {
+		case .http(let statusCode):
+			return "Firebase API call failed with HTTP status code \(statusCode)"
+		}
+	}
+}
