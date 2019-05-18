e370a48144874cc66bfe17a7ff87e4a5295216b8
davdroman <d@vidroman.me>
2019-05-18 22:45:03 +0100

    Remove `*Error.swift` files

--- Sources/Git/Branch.swift
+++ Sources/Git/Branch.swift
@@ -120,3 +120,19 @@ extension Branch {
 		return parentBranch
 	}
 }
+
+extension Branch {
+	public enum Error {
+		case nameContainsInvalidCharacters
+	}
+}
+
+extension Branch.Error: LocalizedError {
+	public var errorDescription: String? {
+		switch self {
+		case .nameContainsInvalidCharacters:
+			return "Name contains invalid characters"
+		}
+	}
+}
+
--- Sources/Git/Commit.swift
+++ Sources/Git/Commit.swift
@@ -37,3 +37,21 @@ extension Commit {
 			.components(separatedBy: "\n")
 	}
 }
+
+extension Commit {
+	public enum Error {
+		case missingProperty
+		case numberNotFound
+	}
+}
+
+extension Commit.Error: LocalizedError {
+	public var errorDescription: String? {
+		switch self {
+		case .missingProperty:
+			return "Could not find required properties for commit in initialization"
+		case .numberNotFound:
+			return "Could not parse number for commit count"
+		}
+	}
+}
