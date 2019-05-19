17eac67a384a6a54ca3c2189c81e3b44bebce9f4
davdroman <d@vidroman.dev>
2019-05-19 04:55:50 +0100

    Add SwiftLint

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,5 +2,8 @@ import Danger
 
 let danger = Danger()
 
-let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-message("These files have changed: \(editedFiles.joined())")
+SwiftLint.lint()
+
+(danger.git.modifiedFiles + danger.git.createdFiles)
+	.filter { $0.contains("Copyright") && $0.fileType == .swift }
+	.forEach { fail(message: "Please remove this copyright header", file: $0, line: 0) }
