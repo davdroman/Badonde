6878ad5627e62b6cafa778ab7888debe24aa2886
davdroman <d@vidroman.dev>
2019-05-24 00:23:52 +0100

    Use PR-only files for copyright notice rule

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -4,13 +4,8 @@ let danger = Danger()
 
 SwiftLint.lint(inline: true, strict: true, lintAllFiles: true)
 
-danger.utils.exec(#"find $PWD -type f -iname "*.swift" | grep -v '.build'"#)
-	.components(separatedBy: "\n")
-	.filter { !$0.isEmpty }
+(danger.git.modifiedFiles + danger.git.createdFiles)
+	.filter { $0.fileType == .swift }
 	.map { (file: $0, contents: danger.utils.readFile($0)) }
 	.filter { $0.contents.contains("//  Created by") }
 	.forEach { fail(message: "Please remove this copyright header", file: $0.file, line: 0) }
-
-//(danger.git.modifiedFiles + danger.git.createdFiles)
-//	.filter { $0.contains("Copyright") && $0.fileType == .swift }
-//	.forEach { fail(message: "Please remove this copyright header", file: $0, line: 0) }
