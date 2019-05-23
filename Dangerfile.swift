6ce733c32fdf4c3a946e8bb7c1ce51d4eb6baaba
davdroman <d@vidroman.me>
2019-05-23 23:50:51 +0100

    Implement draft of copyright notice rule

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -4,6 +4,13 @@ let danger = Danger()
 
 SwiftLint.lint(inline: true, strict: true, lintAllFiles: true)
 
-(danger.git.modifiedFiles + danger.git.createdFiles)
-	.filter { $0.contains("Copyright") && $0.fileType == .swift }
-	.forEach { fail(message: "Please remove this copyright header", file: $0, line: 0) }
+danger.utils.exec(#"find $PWD -type f -iname "*.swift" | grep -v '.build'"#)
+	.components(separatedBy: "\n")
+	.filter { !$0.isEmpty }
+	.map { (file: $0, contents: danger.utils.readFile($0)) }
+	.filter { $0.contents.contains("//  Created by") }
+	.forEach { fail(message: "Please remove this copyright header", file: $0.file, line: 0) }
+
+//(danger.git.modifiedFiles + danger.git.createdFiles)
+//	.filter { $0.contains("Copyright") && $0.fileType == .swift }
+//	.forEach { fail(message: "Please remove this copyright header", file: $0, line: 0) }
