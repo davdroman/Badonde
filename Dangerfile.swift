5fedabdc9a9438c561651298b181736b477f317d
davdroman <d@vidroman.me>
2019-05-23 15:23:30 +0100

    Make linting inline again

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,7 +2,7 @@ import Danger
 
 let danger = Danger()
 
-SwiftLint.lint(inline: false, strict: true, lintAllFiles: true)
+SwiftLint.lint(inline: true, strict: true, lintAllFiles: true)
 
 (danger.git.modifiedFiles + danger.git.createdFiles)
 	.filter { $0.contains("Copyright") && $0.fileType == .swift }
