33bcef37e3bdb4c21c8107150a2c0bf200ff7563
davdroman <d@vidroman.dev>
2019-05-24 10:17:22 +0100

    Fix copyright rule to exclude Dangerfile's own rule

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -7,5 +7,5 @@ SwiftLint.lint(inline: true, strict: true, lintAllFiles: true)
 (danger.git.modifiedFiles + danger.git.createdFiles)
 	.filter { $0.fileType == .swift }
 	.map { (file: $0, contents: danger.utils.readFile($0)) }
-	.filter { $0.contents.contains("//  Created by") }
+	.filter { $0.contents.contains("\n//  Created by") }
 	.forEach { fail(message: "Please remove this copyright header", file: $0.file, line: 0) }
