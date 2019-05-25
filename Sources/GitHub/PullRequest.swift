31c6a7a4f0920ccf7ddae56ef80555432db95b0c
davdroman <d@vidroman.dev>
2019-05-25 02:27:46 +0100

    Add `url` property to `PullRequest`

--- Sources/GitHub/PullRequest.swift
+++ Sources/GitHub/PullRequest.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 public class PullRequest: Decodable {
+	public var url: URL
 	public var number: Int
+
+	enum CodingKeys: String, CodingKey {
+		case url = "html_url"
+		case number
+	}
 }
