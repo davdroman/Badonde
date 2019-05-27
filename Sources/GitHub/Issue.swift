64e980843a6e68bb82b1ac18fca41532fb252801
davdroman <d@vidroman.me>
2019-05-27 16:21:26 +0100

    Gardening

--- Sources/GitHub/Issue.swift
+++ Sources/GitHub/Issue.swift
@@ -1,9 +1,11 @@
 import Foundation
 
-public class Issue: Decodable {
+public struct Issue {
 	public var url: URL
 	public var number: Int
+}
 
+extension Issue: Decodable {
 	enum CodingKeys: String, CodingKey {
 		case url = "html_url"
 		case number
