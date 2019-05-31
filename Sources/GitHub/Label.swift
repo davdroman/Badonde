c536b71a40964d5e3b90495f6e9c2ab8a076c202
davdroman <d@vidroman.dev>
2019-05-31 15:58:08 +0100

    Implement `BadondeKit.Output.PullRequest`

--- Sources/GitHub/Label.swift
+++ Sources/GitHub/Label.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-public struct Label: Decodable {
+public struct Label: Codable {
 	public let name: String
 }
--- Sources/GitHub/Milestone.swift
+++ Sources/GitHub/Milestone.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct Milestone: Decodable {
+public struct Milestone: Codable {
 	public let number: Int
 	public let title: String
 }
