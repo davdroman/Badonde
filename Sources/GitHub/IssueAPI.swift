d57b70c2a3c5b3445fd564db4d916f7305c18b9d
davdroman <d@vidroman.dev>
2019-05-31 15:56:26 +0100

    Swap `GitHub.Repository` ~> `Git.Repository`

--- Sources/GitHub/IssueAPI.swift
+++ Sources/GitHub/IssueAPI.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Git
 
 extension Issue {
 	public final class API: GitHub.API {
--- Sources/GitHub/LabelAPI.swift
+++ Sources/GitHub/LabelAPI.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Git
 
 extension Label {
 	public final class API: GitHub.API {
