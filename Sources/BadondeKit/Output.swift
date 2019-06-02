9a055afc057a653d9ad4b94eb9df2bd9958913d3
davdroman <d@vidroman.me>
2019-06-03 00:08:36 +0100

    Implement `BadondeKit.output` singleton + interactor functions

--- Sources/BadondeKit/Output.swift
+++ Sources/BadondeKit/Output.swift
@@ -24,3 +24,52 @@ extension Output {
 		return FileManager.default.temporaryDirectory.appendingPathComponent("badonde-output-\(repositoryPathSha1).json")
 	}
 }
+
+var output: Output? {
+	get {
+		do {
+			let repository = try Repository()
+			let outputPath = Output.path(for: repository)
+			let outputData = try Data(contentsOf: outputPath)
+			let output = try JSONDecoder().decode(Output.self, from: outputData)
+			return output
+		} catch {
+			return nil
+		}
+	}
+	set {
+		guard let newValue = newValue else {
+			return
+		}
+		do {
+			let repository = try Repository()
+			let outputPath = Output.path(for: repository)
+			let outputData = try JSONEncoder().encode(newValue)
+			try outputData.write(to: outputPath)
+		} catch { }
+	}
+}
+
+public func title(_ title: String) {
+	output?.pullRequest.title = title
+}
+
+public func body(_ body: String) {
+	output?.pullRequest.body = body
+}
+
+public func assignee(_ assignee: String) {
+	output?.pullRequest.assignees = output?.pullRequest.assignees ?? [] + [assignee]
+}
+
+public func label(_ label: Label) {
+	output?.pullRequest.labels = output?.pullRequest.labels ?? [] + [label]
+}
+
+public func milestone(_ milestone: Milestone) {
+	output?.pullRequest.milestone = milestone
+}
+
+public func draft(_ isDraft: Bool) {
+	output?.pullRequest.isDraft = isDraft
+}
