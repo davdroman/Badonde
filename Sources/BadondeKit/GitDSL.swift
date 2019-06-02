3401531bef98296f3a1b1a06c5219f8f3ee33a56
davdroman <d@vidroman.dev>
2019-06-03 00:09:06 +0100

    Implement DSLs

---
+++ Sources/BadondeKit/GitDSL.swift
@@ -0,0 +1,8 @@
+import Foundation
+import Git
+
+public struct GitDSL {
+	public var remote: Remote
+	public var defaultBranch: Branch
+	public var currentBranch: Branch
+}
---
+++ Sources/BadondeKit/GitHubDSL.swift
@@ -0,0 +1,6 @@
+import Foundation
+import GitHub
+
+public struct GitHubDSL {
+
+}
