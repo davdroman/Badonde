4f38be3a7020e0d386d36e3ebc1a060dadf77f2a
davdroman <d@vidroman.dev>
2019-06-03 23:00:53 +0100

    Optimize `GitHub.Label.API.allLabels` time

--- Sources/GitHub/LabelAPI.swift
+++ Sources/GitHub/LabelAPI.swift
@@ -7,10 +7,13 @@ extension Label {
 			super.init(authorization: .token(accessToken))
 		}
 
-		public func labels(for shorthand: Repository.Shorthand, page: Int) throws -> [Label] {
+		public func labels(for shorthand: Repository.Shorthand, page: Int?, perPage: Int? = nil) throws -> [Label] {
 			return try get(
 				endpoint: "/repos/\(shorthand.rawValue)/labels",
-				queryItems: [URLQueryItem(name: "page", value: String(page))],
+				queryItems: [
+					URLQueryItem(name: "page", mandatoryValue: page.map(String.init)),
+					URLQueryItem(name: "per_page", mandatoryValue: perPage.map(String.init)),
+				].compacted(),
 				responseType: [Label].self
 			)
 		}
@@ -18,12 +21,13 @@ extension Label {
 		public func allLabels(for shorthand: Repository.Shorthand) throws -> [Label] {
 			var labels: [Label] = []
 			var currentPage = 1
+			let resultsPerPage = 100
 			while true {
-				let newLabels = try self.labels(for: shorthand, page: currentPage)
-				guard !newLabels.isEmpty else {
+				let newLabels = try self.labels(for: shorthand, page: currentPage, perPage: resultsPerPage)
+				labels += newLabels
+				guard newLabels.count == resultsPerPage else {
 					break
 				}
-				labels += newLabels
 				currentPage += 1
 			}
 			return labels
