c729c576b2d9cb0453736fa4d5672912ea7aac28
davdroman <d@vidroman.dev>
2019-06-11 18:33:47 +0100

    Implement `BadondeKit.GitHubDSL.me`

--- Sources/BadondeKit/GitHubDSL.swift
+++ Sources/BadondeKit/GitHubDSL.swift
@@ -2,6 +2,7 @@ import Foundation
 import GitHub
 
 public struct GitHubDSL {
+	public var me: User!
 	public var labels: [Label]
 	public var milestones: [Milestone]
 	public var openPullRequests: [PullRequest]
