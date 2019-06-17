16f48b6d178975cd1ab1db5966cf412613d65e99
davdroman <d@vidroman.dev>
2019-06-17 17:09:15 +0100

    Use paths instead of urls in `Configuration` module

--- Tests/ConfigurationTests/ConfigurationTests.swift
+++ Tests/ConfigurationTests/ConfigurationTests.swift
@@ -3,7 +3,7 @@ import XCTest
 import TestSugar
 
 final class JSONFileInteractorSpy: JSONFileInteractor {
-	typealias WriteSpy = ([String: Any], URL) -> Void
+	typealias WriteSpy = (_ rawObject: [String: Any], _ path: String) -> Void
 
 	var readFixture: FixtureLoadable
 	var writeSpy: WriteSpy
@@ -13,12 +13,12 @@ final class JSONFileInteractorSpy: JSONFileInteractor {
 		self.writeSpy = writeSpy
 	}
 
-	func read(from url: URL) throws -> [String: Any] {
+	func read(from path: String) throws -> [String: Any] {
 		return try readFixture.load(as: [String: Any].self)
 	}
 
-	func write(_ rawObject: [String: Any], to url: URL) throws {
-		writeSpy(rawObject, url)
+	func write(_ rawObject: [String: Any], to path: String) throws {
+		writeSpy(rawObject, path)
 	}
 }
 
@@ -34,47 +34,47 @@ final class ConfigurationTests: XCTestCase {
 	}
 
 	func testInit_withEmptyFile() throws {
-		let config = try Configuration(contentsOf: Fixture.empty.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.empty.path, supportedKeyPaths: [])
 		XCTAssertTrue(config.rawObject.isEmpty)
 	}
 
 	func testInit_withEmptyArrayFile() throws {
-		let config = try Configuration(contentsOf: Fixture.emptyArray.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.emptyArray.path, supportedKeyPaths: [])
 		XCTAssertTrue(config.rawObject.isEmpty)
 	}
 
 	func testInit_withEmptyDictionaryFile() throws {
-		let config = try Configuration(contentsOf: Fixture.emptyDictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.emptyDictionary.path, supportedKeyPaths: [])
 		XCTAssertTrue(config.rawObject.isEmpty)
 	}
 
 	func testInit_withArrayFile() throws {
-		let config = try Configuration(contentsOf: Fixture.array.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.array.path, supportedKeyPaths: [])
 		XCTAssertTrue(config.rawObject.isEmpty)
 	}
 }
 
 extension ConfigurationTests {
 	func testGetRawValue_ofStringValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getRawValue(forKeyPath: .name)
 		XCTAssertEqual(value, "David")
 	}
 
 	func testGetRawValue_ofIntValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getRawValue(forKeyPath: .age)
 		XCTAssertEqual(value, "21")
 	}
 
 	func testGetRawValue_ofDoubleValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getRawValue(forKeyPath: .weight)
 		XCTAssertEqual(value, "72.5")
 	}
 
 	func testGetRawValue_ofBoolValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 
 		let valueA = try config.getRawValue(forKeyPath: .likesPepperoniPizza)
 		XCTAssertEqual(valueA, "true")
@@ -84,13 +84,13 @@ extension ConfigurationTests {
 	}
 
 	func testGetRawValue_ofArrayValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getRawValue(forKeyPath: .favoriteMovies)
 		XCTAssertNil(value)
 	}
 
 	func testGetRawValue_ofStringValue_withNestedKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 
 		let valueA = try config.getRawValue(forKeyPath: .bankDetails)
 		XCTAssertNil(valueA)
@@ -103,13 +103,13 @@ extension ConfigurationTests {
 	}
 
 	func testGetRawValue_ofNullType_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getRawValue(forKeyPath: "iq")
 		XCTAssertNil(value)
 	}
 
 	func testGetRawValue_withMissingPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getRawValue(forKeyPath: .likesGazpacho)
 		XCTAssertNil(value)
 	}
@@ -117,25 +117,25 @@ extension ConfigurationTests {
 
 extension ConfigurationTests {
 	func testGetValue_ofStringValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getValue(ofType: String.self, forKeyPath: .name)
 		XCTAssertEqual(value, "David")
 	}
 
 	func testGetValue_ofIntValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getValue(ofType: Int.self, forKeyPath: .age)
 		XCTAssertEqual(value, 21)
 	}
 
 	func testGetValue_ofDoubleValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getValue(ofType: Double.self, forKeyPath: .weight)
 		XCTAssertEqual(value, 72.5)
 	}
 
 	func testGetValue_ofBoolValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 
 		let valueA = try config.getValue(ofType: Bool.self, forKeyPath: .likesPepperoniPizza)
 		XCTAssertEqual(valueA, true)
@@ -145,13 +145,13 @@ extension ConfigurationTests {
 	}
 
 	func testGetValue_ofArrayValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getValue(ofType: [String].self, forKeyPath: .favoriteMovies)
 		XCTAssertNil(value)
 	}
 
 	func testGetValue_ofStringValue_withNestedKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 
 		let valueA = try config.getValue(ofType: String.self, forKeyPath: .bankDetails)
 		XCTAssertNil(valueA)
@@ -164,19 +164,19 @@ extension ConfigurationTests {
 	}
 
 	func testGetValue_ofNullType_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getValue(ofType: Int.self, forKeyPath: "iq")
 		XCTAssertNil(value)
 	}
 
 	func testGetValue_withMissingPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		let value = try config.getValue(ofType: Bool.self, forKeyPath: .likesGazpacho)
 		XCTAssertNil(value)
 	}
 
 	func testGetValue_ofInvalidBoolValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		XCTAssertThrowsError(try config.getValue(ofType: Bool.self, forKeyPath: .name)) { error in
 			switch error {
 			case let Configuration.Error.typeBridgingFailed(value, type):
@@ -189,7 +189,7 @@ extension ConfigurationTests {
 	}
 
 	func testGetValue_ofInvalidDoubleValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		XCTAssertThrowsError(try config.getValue(ofType: Double.self, forKeyPath: .name)) { error in
 			switch error {
 			case let Configuration.Error.typeBridgingFailed(value, type):
@@ -202,7 +202,7 @@ extension ConfigurationTests {
 	}
 
 	func testGetValue_ofInvalidIntValue_withPlainKey() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		XCTAssertThrowsError(try config.getValue(ofType: Int.self, forKeyPath: .name)) { error in
 			switch error {
 			case let Configuration.Error.typeBridgingFailed(value, type):
@@ -215,7 +215,7 @@ extension ConfigurationTests {
 	}
 
 	func testGetValue_ofInvalidBridgingType() throws {
-		let config = try Configuration(contentsOf: Fixture.dictionary.url, supportedKeyPaths: [])
+		let config = try Configuration(contentsOfFile: Fixture.dictionary.path, supportedKeyPaths: [])
 		XCTAssertThrowsError(try config.getValue(ofType: Date.self, forKeyPath: .name)) { error in
 			switch error {
 			case let Configuration.Error.invalidBridgingType(type):
@@ -233,7 +233,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setValue(5, forKeyPath: .socialSecurityNumber)
 		let value = try config.getValue(ofType: Int.self, forKeyPath: .socialSecurityNumber)
@@ -247,7 +247,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setValue("Jack", forKeyPath: .name)
 		let value = try config.getValue(ofType: String.self, forKeyPath: .name)
@@ -261,7 +261,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setValue(25, forKeyPath: .age)
 		let value = try config.getValue(ofType: Int.self, forKeyPath: .age)
@@ -275,7 +275,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setValue(73.5, forKeyPath: .weight)
 		let value = try config.getValue(ofType: Double.self, forKeyPath: .weight)
@@ -289,7 +289,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setValue(false, forKeyPath: .likesPepperoniPizza)
 		let value = try config.getValue(ofType: Bool.self, forKeyPath: .likesPepperoniPizza)
@@ -301,7 +301,7 @@ extension ConfigurationTests {
 	func testSetValue_ofArrayValue_withPlainKey() throws {
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in XCTFail("Config should not be written to") }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		XCTAssertThrowsError(
 			try config.setValue(["The Devil Wears Prada"], forKeyPath: .favoriteMovies)
@@ -320,7 +320,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setValue("123456", forKeyPath: .bankDetailsAccountNumber)
 		let value = try config.getValue(ofType: String.self, forKeyPath: .bankDetailsAccountNumber)
@@ -334,7 +334,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setValue("123", forKeyPath: .bankDetailsAccount)
 		let value = try config.getValue(ofType: String.self, forKeyPath: .bankDetailsAccount)
@@ -346,7 +346,7 @@ extension ConfigurationTests {
 	func testSetValue_ofStringValue_withParentOfNestedKey() throws {
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in XCTFail("Config should not be written to") }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		XCTAssertThrowsError(
 			try config.setValue(3, forKeyPath: .bankDetails)
@@ -367,7 +367,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setRawValue("Jack", forKeyPath: .name)
 		let value = try config.getValue(ofType: String.self, forKeyPath: .name)
@@ -381,7 +381,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setRawValue("25", forKeyPath: .age)
 		let value = try config.getValue(ofType: Int.self, forKeyPath: .age)
@@ -395,7 +395,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setValue("70.3", forKeyPath: .weight)
 		let value = try config.getValue(ofType: Double.self, forKeyPath: .weight)
@@ -410,7 +410,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.setRawValue("false", forKeyPath: .likesPepperoniPizza)
 		let valueA = try config.getValue(ofType: Bool.self, forKeyPath: .likesPepperoniPizza)
@@ -430,7 +430,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.removeValue(forKeyPath: .likesPineapplePizza)
 		let value = try config.getValue(ofType: Bool.self, forKeyPath: .likesPineapplePizza)
@@ -444,7 +444,7 @@ extension ConfigurationTests {
 
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in expectation.fulfill() }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		try config.removeValue(forKeyPath: .bankDetailsAccountNumber)
 		let value = try config.getValue(ofType: Bool.self, forKeyPath: .bankDetailsAccountNumber)
@@ -456,7 +456,7 @@ extension ConfigurationTests {
 	func testRemoveValue_withParentOfNestedKey() throws {
 		let fixture = Fixture.dictionary
 		let interactor = JSONFileInteractorSpy(readFixture: fixture) { _, _ in XCTFail("Config should not be written to") }
-		let config = try Configuration(contentsOf: fixture.url, supportedKeyPaths: [], fileInteractor: interactor)
+		let config = try Configuration(contentsOfFile: fixture.path, supportedKeyPaths: [], fileInteractor: interactor)
 
 		XCTAssertThrowsError(
 			try config.removeValue(forKeyPath: .bankDetails)
