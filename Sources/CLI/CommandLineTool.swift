b1b72a5009e299c7a3daef14fbc9e7e061b6353c
davdroman <d@vidroman.me>
2019-06-22 22:51:21 +0100

    Bump version number (2.0.0-beta.3)

--- Sources/CLI/CommandLineTool.swift
+++ Sources/CLI/CommandLineTool.swift
@@ -7,7 +7,7 @@ import Git
 public final class CommandLineTool {
 	enum Constant {
 		static let name = "badonde"
-		static let version = "2.0.0-beta.2"
+		static let version = "2.0.0-beta.3"
 		static let description = "Painless PR-ing"
 	}
 
