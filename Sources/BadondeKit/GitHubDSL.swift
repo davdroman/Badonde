8d72493f532493137744ee005bfe2d5b285663f5
davdroman <d@vidroman.me>
2019-06-24 14:36:16 +0100

    Document `GitHubDSL.swift`

--- Sources/BadondeKit/GitHubDSL.swift
+++ Sources/BadondeKit/GitHubDSL.swift
@@ -1,9 +1,14 @@
 import Foundation
 import GitHub
 
+/// An object representing Badonde's current GitHub context.
 public struct GitHubDSL {
+	/// The GitHub user who's running Badonde.
 	public var me: User!
+	/// All available repo labels.
 	public var labels: [Label]
+	/// All active repo milestones.
 	public var milestones: [Milestone]
+	/// All open pull requests in the repo.
 	public var openPullRequests: [PullRequest]
 }
