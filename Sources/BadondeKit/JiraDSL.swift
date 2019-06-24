6fbac7c1a59eecc410f7464adad39a419b8e967a
davdroman <d@vidroman.dev>
2019-06-24 14:37:22 +0100

    Document `JiraDSL.swift`

--- Sources/BadondeKit/JiraDSL.swift
+++ Sources/BadondeKit/JiraDSL.swift
@@ -1,6 +1,8 @@
 import Foundation
 import Jira
 
+/// An object representing Badonde's current Jira context.
 public struct JiraDSL {
+	/// The Jira ticket for the current branch.
 	public var ticket: Ticket
 }
