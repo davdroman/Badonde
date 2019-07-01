f827122e7579a93e1f71545b93670a19bbd97f50
davdroman <d@vidroman.dev>
2019-07-01 19:09:44 +0100

    Bump version number (2.0.0-beta.7)

--- Sources/CLI/CommandLineTool.swift
+++ Sources/CLI/CommandLineTool.swift
@@ -7,7 +7,7 @@ import Git
 public final class CommandLineTool {
 	enum Constant {
 		static let name = "badonde"
-		static let version = "2.0.0-beta.6"
+		static let version = "2.0.0-beta.7"
 	}
 
 	var startDate = Date()
