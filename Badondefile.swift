b0edc08b9595d71f79ed5ee40276160f012a4062
davdroman <d@vidroman.me>
2019-07-04 21:15:19 +0100

    Automate Badonde's release PR workflow

--- Badondefile.swift
+++ Badondefile.swift
@@ -5,16 +5,26 @@ let badonde = Badonde(
 	baseBranchDerivationStrategy: .commitProximity
 )
 
-// Set issue to convert to PR (if found).
-if let githubIssue = badonde.github.issue {
-	issue(githubIssue.number)
-} else {
-	// If issue not found, prettify PR title as follows:
-	// "implement-something-cool" ~> "Implement something cool"
-	title({
-		let temp = badonde.git.currentBranch.name.replacingOccurrences(of: "-", with: " ")
-		return temp.prefix(1).localizedCapitalized + temp.dropFirst()
-	}())
+switch (badonde.pullRequest.baseBranch, badonde.pullRequest.headBranch) {
+case ("master", "develop"):
+	let latestTag = badonde.git.tags[0]
+	title("\(latestTag) release")
+case (_, "develop"):
+	Logger.failAndExit("Develop should only be merged into master")
+case (_, "master"):
+	Logger.failAndExit("Running Badonde from master is not allowed")
+default:
+	// Set issue to convert to PR (if found).
+	if let githubIssue = badonde.github.issue {
+		issue(githubIssue.number)
+	} else {
+		// If issue not found, prettify PR title as follows:
+		// "implement-something-cool" ~> "Implement something cool"
+		title({
+			let temp = badonde.git.currentBranch.name.replacingOccurrences(of: "-", with: " ")
+			return temp.prefix(1).localizedCapitalized + temp.dropFirst()
+		}())
+	}
 }
 
 // Add a cheeky little watermark at the end :)
